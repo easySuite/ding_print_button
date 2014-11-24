bd596886912b80c42bcde7b7a5c35afbd02c00cf
amd_k8 <am@inlead.dk>
2014-11-24 11:45:23 +0200

    Code review changes.

--- js/ding_print_button.js
+++ js/ding_print_button.js
@@ -1,10 +1,11 @@
+/**
+ * @file
+ *
+ */
 (function($) {
   'use strict';
 
-  // On document ready.
   $(function() {
-
-    // Print button functionality.
     $('.print-button').click(function(e) {
       e.preventDefault();
       window.print();
